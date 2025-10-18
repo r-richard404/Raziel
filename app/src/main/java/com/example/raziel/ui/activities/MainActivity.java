2f73360eace78f3536479aa0a8ebf9175c4e1040
Richard <r.lazarica@yahoo.com>
2025-10-18 18:29:00 +0100

    Created basic activity layout to test first encryption algorithm with buttons and other indicators

--- app/src/main/java/com/example/raziel/ui/activities/MainActivity.java
+++ app/src/main/java/com/example/raziel/ui/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.raziel;
+package com.example.raziel.ui.activities;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.raziel.R;
 
 public class MainActivity extends AppCompatActivity {
 
