79ab5f899318711a91c15a386a33a8f3b522d421
Richard <r.lazarica@yahoo.com>
2025-10-20 01:39:54 +0100

    Update internal file manager to match new methods in interface

--- app/src/main/java/com/example/raziel/core/managers/file/InternalFileManager.java
+++ app/src/main/java/com/example/raziel/core/managers/file/InternalFileManager.java
@@ -1,11 +1,14 @@
 package com.example.raziel.core.managers.file;
 
 import android.content.Context;
+import android.net.Uri;
 
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.Files;
 
 
@@ -13,10 +16,21 @@ import java.nio.file.Files;
 
 public class InternalFileManager implements FileManager {
     private final Context context;
-
+    Uri currentInputStream;
+    Uri currentOutputStream;
 
     public InternalFileManager(Context context) {
         this.context = context;
+    }
+
+
+    public void currentInputStream(Uri uri) {
+        this.currentInputStream = uri;
+    }
+
+
+    public void setCurrentOutputStream(Uri uri) {
+        this.currentOutputStream = uri;
     }
 
 
@@ -34,6 +48,24 @@ public class InternalFileManager implements FileManager {
        try(FileOutputStream fos = new FileOutputStream(file)) {
             fos.write(data);
        }
+    }
+
+
+    @Override
+    public InputStream getInputStream(String filePath) throws IOException {
+        if(currentInputStream == null) {
+            throw new IOException("No input file selected");
+        }
+        return context.getContentResolver().openInputStream(currentInputStream);
+    }
+
+
+    @Override
+    public OutputStream getOutputStream(String filePath) throws IOException {
+        if(currentOutputStream == null) {
+            throw new IOException("No output file selected");
+        }
+        return context.getContentResolver().openOutputStream(currentOutputStream);
     }
 
 
